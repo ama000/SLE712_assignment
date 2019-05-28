a326691ea261392fb44505855d64bf93214448f8
markziemann <mark.ziemann@gmail.com>
2019-05-28 12:57:47 +1000

    update

--- myscript.sh
+++ myscript.sh
@@ -3,8 +3,8 @@
 # in case we need to reindex the db
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
-# incorporate some mismatches
-msbar -sequence mysample.fa -count 10 -point 4 -block 0 -codon 0  -o -outseq mysample_mutated.fa
+#incorporate some mismatches
+msbar -sequence 11.fa -count 10 -point 4 -block 0 -codon 0  -o -outseq mysample_mutated.fa
 
 # run the blasts
  blast2 -m 8 -p blastn -e 0.001 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
